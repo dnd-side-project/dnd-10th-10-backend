61d0775684a3d2f26061b2881698b7106210882c
JeongMin <skawjdals96@gmail.com>
2024-01-29 23:54:55 +0900

    ✨feature: DomainApplication 클래스 생성

---
+++ basterdz-domain/src/main/java/com/dnd/BasterdzDomainApplication.java
@@ -0,0 +1,11 @@
+package com.dnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasterdzDomainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BasterdzDomainApplication.class, args);
+    }
+}
