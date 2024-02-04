b92d3449ce7c6f50f296a82be38add85d0bdb3af
stophwan <jjw981210@gmail.com>
2024-02-05 03:48:40 +0900

    :recycle: refactor: swagger ApiResponse 추가

--- basterdz-api/src/main/java/com/dnd/domains/room/controller/RoomApiPresentation.java
+++ basterdz-api/src/main/java/com/dnd/domains/room/controller/RoomApiPresentation.java
@@ -5,11 +5,13 @@ import com.dnd.domains.room.dto.request.RoomCreateRequestDto;
 import com.dnd.domains.room.dto.response.RoomCreateResponseDto;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Room", description = "Room API")
 public interface RoomApiPresentation {
 
 	@Operation(summary = "목표 방 생성")
+	@ApiResponse(responseCode = "200", description = "방 생성 성공")
 	ApiResult<RoomCreateResponseDto> createRoom(final RoomCreateRequestDto roomCreateRequestDto);
 }
