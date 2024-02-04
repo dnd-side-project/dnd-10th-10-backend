eb7015b6029ee84f18fcc8ab92c5b327e8494898
JeongMin <skawjdals96@gmail.com>
2024-02-05 03:19:58 +0900

    ✅ 🔧 chore: H2 환경설정 추가

--- basterdz-api/src/test/java/com/dnd/domains/room/service/RoomServiceTest.java
+++ basterdz-api/src/test/java/com/dnd/domains/room/service/RoomServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 
@@ -18,6 +19,7 @@ import static com.dnd.room.vo.RestrictApp.INSTAGRAM;
 import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RoomServiceTest {
 
     @Autowired
