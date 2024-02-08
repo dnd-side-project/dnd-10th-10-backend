f4be1d40a3f3c876a7e6d64c9543ae15f3e9c83d
stophwan <jjw981210@gmail.com>
2024-02-09 03:58:12 +0900

    :sparkles: feat: ScreenReport swagger 작성

---
+++ basterdz-api/src/main/java/com/dnd/api/domains/report/controller/ScreenReportApiPresentation.java
@@ -0,0 +1,16 @@
+package com.dnd.api.domains.report.controller;
+
+import com.dnd.api.common.dto.ApiResult;
+import com.dnd.api.domains.report.dto.ScreenReportRequest;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Report", description = "Report API")
+public interface ScreenReportApiPresentation {
+
+	@Operation(summary = "하루 스크린 리포트 전송")
+	@ApiResponse(responseCode = "204", description = "스크린 리포트 요청 성공")
+	ApiResult<Void> appendScreenReport(ScreenReportRequest screenReportRequest);
+}
