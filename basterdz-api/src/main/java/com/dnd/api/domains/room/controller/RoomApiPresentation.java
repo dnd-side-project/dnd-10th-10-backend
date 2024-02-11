6bc96904d3d1b7181a1f8336bf0bc2df35be494b
JeongMin <skawjdals96@gmail.com>
2024-02-12 03:03:37 +0900

    ♻ refactor: RoomController와 RoomMemberController 분리

--- basterdz-api/src/main/java/com/dnd/api/domains/room/controller/RoomApiPresentation.java
+++ basterdz-api/src/main/java/com/dnd/api/domains/room/controller/RoomApiPresentation.java
@@ -1,14 +1,9 @@
 package com.dnd.api.domains.room.controller;
 
-import com.dnd.api.domains.room.dto.CreateRoomRequest;
-import com.dnd.api.domains.room.dto.EnterRoomRequest;
 import com.dnd.api.domains.room.dto.FindRoomResponse;
 import com.dnd.api.domains.room.dto.FindRoomByCodeResponse;
 
-import org.springframework.http.ResponseEntity;
-
 import com.dnd.api.common.dto.ApiResult;
-import com.dnd.api.domains.room.dto.RoomIdResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -20,48 +15,27 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "\uD83C\uDFE0Room", description = "Room API")
 public interface RoomApiPresentation {
 
-	@Operation(summary = "목표 방 생성")
-	@ApiResponse(responseCode = "201", description = "방 생성 성공")
-	ResponseEntity<ApiResult<RoomIdResponse>> createRoom(final CreateRoomRequest roomCreateRequestDto);
-
 	@Operation(summary = "목표 방 조회")
 	@ApiResponses(
-		value = {
-			@ApiResponse(responseCode = "200", description = "방 조회 성공"),
-			@ApiResponse(responseCode = "404", description = "존재하지 않는 그룹",
-				content = @Content(schema = @Schema(
-					example = "{\"success\": false, \"data\" : null,"
-						+ "\"error\": {\"code\": \"ROOM-01\", \"message\": \"존재하지 않는 그룹입니다.\"}}")))
-		}
+			value = {
+					@ApiResponse(responseCode = "200", description = "방 조회 성공"),
+					@ApiResponse(responseCode = "404", description = "존재하지 않는 그룹",
+							content = @Content(schema = @Schema(
+									example = "{\"success\": false, \"data\" : null,"
+											+ "\"error\": {\"code\": \"ROOM-01\", \"message\": \"존재하지 않는 그룹입니다.\"}}")))
+			}
 	)
 	ApiResult<FindRoomResponse> findRoom(final Long roomId);
 
 	@Operation(summary = "초대 코드를 통한 목표 방 조회")
 	@ApiResponses(
-		value = {
-			@ApiResponse(responseCode = "200", description = "방 조회 성공"),
-			@ApiResponse(responseCode = "400", description = "유효하지 않은 초대코드",
-				content = @Content(schema = @Schema(
-					example = "{\"success\": false, \"data\" : null,"
-						+ "\"error\": {\"code\": \"ROOM-02\", \"message\": \"유효하지 않은 초대코드입니다.\"}}")))
-		}
+			value = {
+					@ApiResponse(responseCode = "200", description = "방 조회 성공"),
+					@ApiResponse(responseCode = "400", description = "유효하지 않은 초대코드",
+							content = @Content(schema = @Schema(
+									example = "{\"success\": false, \"data\" : null,"
+											+ "\"error\": {\"code\": \"ROOM-02\", \"message\": \"유효하지 않은 초대코드입니다.\"}}")))
+			}
 	)
 	ApiResult<FindRoomByCodeResponse> findRoomByInviteCode(final String inviteCode);
-
-	@Operation(summary = "목표 방 입장")
-	@ApiResponses(
-		value = {
-			@ApiResponse(responseCode = "200", description = "방 입장 성공"),
-			@ApiResponse(responseCode = "400", description = "유효하지 않은 초대코드",
-				content = @Content(schema = @Schema(
-					example = "{\"success\": false, \"data\" : null,"
-				+ "\"error\": {\"code\": \"ROOM-02\", \"message\": \"유효하지 않은 초대코드입니다.\"}}"))),
-			@ApiResponse(responseCode = "420", description = "정원 초과",
-				content = @Content(schema = @Schema(
-					example = "{\"success\": false, \"data\" : null,"
-						+ "\"error\": {\"code\": \"ROOM-03\", \"message\": \"정원을 초과하였습니다\"}}")))
-
-		}
-	)
-	ApiResult<RoomIdResponse> enterRoom(final EnterRoomRequest enterRoomRequestDto);
 }
--- basterdz-api/src/main/java/com/dnd/api/domains/room/controller/RoomController.java
+++ basterdz-api/src/main/java/com/dnd/api/domains/room/controller/RoomController.java
@@ -1,23 +1,14 @@
 package com.dnd.api.domains.room.controller;
 
-import static org.springframework.http.HttpStatus.CREATED;
-
 import com.dnd.api.common.dto.ApiResult;
 import com.dnd.api.domains.room.service.RoomService;
-import com.dnd.api.domains.room.dto.CreateRoomRequest;
-import com.dnd.api.domains.room.dto.EnterRoomRequest;
-import com.dnd.api.domains.room.dto.RoomIdResponse;
 import com.dnd.api.domains.room.dto.FindRoomByCodeResponse;
 import com.dnd.api.domains.room.dto.FindRoomResponse;
 import com.dnd.domain.room.entity.Room;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/v1/rooms")
@@ -25,16 +16,6 @@ import java.time.LocalDate;
 public class RoomController implements RoomApiPresentation {
 
 	private final RoomService roomService;
-
-	@PostMapping
-	public ResponseEntity<ApiResult<RoomIdResponse>> createRoom(
-			final @Valid @RequestBody CreateRoomRequest roomCreateRequestDto
-	) {
-		LocalDate registeredDate = LocalDate.now();
-		Room createdRoom = roomService.createRoom(roomCreateRequestDto, registeredDate);
-		RoomIdResponse responseDto = RoomIdResponse.from(createdRoom);
-		return ResponseEntity.status(CREATED).body(ApiResult.ok(responseDto));
-	}
 
 	@GetMapping("/{roomId}")
 	public ApiResult<FindRoomResponse> findRoom(
@@ -52,14 +33,5 @@ public class RoomController implements RoomApiPresentation {
 		Room room = roomService.findRoomByInviteCode(inviteCode);
 		FindRoomByCodeResponse responseDto = FindRoomByCodeResponse.from(room);
 		return ApiResult.ok(responseDto);
-	}
-
-	@PostMapping("/entrance")
-	public ApiResult<RoomIdResponse> enterRoom(
-			final @RequestBody @Valid EnterRoomRequest enterRoomRequestDto
-	) {
-		String inviteCode = enterRoomRequestDto.getInviteCode();
-		Room room = roomService.enterRoom(inviteCode);
-		return ApiResult.ok(RoomIdResponse.from(room));
 	}
 }
