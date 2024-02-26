ceefc8425a0024bfdbaafd83eec788c124a3d751
JeongMin <skawjdals96@gmail.com>
2024-02-27 01:04:32 +0900

    🐛 fix: 빌드 오류 수정

--- basterdz-api/src/test/java/com/dnd/BasterdzApiApplicationTests.java
+++ basterdz-api/src/test/java/com/dnd/BasterdzApiApplicationTests.java
@@ -2,7 +2,6 @@ package com.dnd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 public class BasterdzApiApplicationTests {
--- basterdz-api/src/test/java/com/dnd/api/auth/apple/AppleClientTest.java
+++ basterdz-api/src/test/java/com/dnd/api/auth/apple/AppleClientTest.java
@@ -7,8 +7,10 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AppleClientTest {
 
 	@Autowired
